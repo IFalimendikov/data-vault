e52dedb8b00c70aea1ce07597bf929b6e54771b3
IFalimendikov <golovanjot2@gmail.com>
2025-08-27 15:44:21 +0200

    init

--- client/internal/services/ping_server.go
+++
@@ -1,8 +0,0 @@
-package services
-
-import "context"
-
-// PingServer checks if the server connection is alive
-func (v *Vault) PingServer(ctx context.Context) bool {
-	return v.grpcclient.PingServer(ctx)
-}
